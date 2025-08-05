621fd27e78141bd4b29cebdafaea81b577b0fdfd
Craig P. Motlin <cmotlin@gmail.com>
2025-08-04 21:16:32 -0400

    Use build-ci task in precommit recipe.

--- src/app/components/TimelineSection.tsx
+++ src/app/components/TimelineSection.tsx
@@ -1,5 +1,4 @@
-import * as React from 'react';
-const {useState} = React;
+import {useState} from 'react';
 import type {Game, Mission, Proposal} from '../models/game';
 
 export interface TimelineSectionProps {
