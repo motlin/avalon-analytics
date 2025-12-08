70d632f0f2ebc9515e29577bf3ba7e0a487f8266
Craig P. Motlin <cmotlin@gmail.com>
2025-12-07 19:14:40 -0500

    Revert "Fix: initialize database unconditionally in middleware."
    This reverts commit 6cd16b37bf66c4a6db8336720e128260c84628f0.

--- src/worker.tsx
+++ src/worker.tsx
@@ -26,7 +26,8 @@ export default defineApp([
 	setCommonHeaders(),
 	async ({ctx, request, response}) => {
 		console.log('🔍 Worker env keys:', Object.keys(env));
-		await setupDb(env);
+		// Note: setupDb() is NOT called here to avoid Prisma WASM loading issues
+		// It will be called lazily when db access is actually needed
 		setupFirestoreRestService(env);
 
 		// Only set up sessions if AUTH_SECRET_KEY is configured
@@ -52,7 +53,9 @@ export default defineApp([
 		}
 
 		if (ctx.session?.userId) {
+			// Lazy initialize Prisma only when we need to look up the user
 			try {
+				await setupDb(env);
 				ctx.user = await db.user.findUnique({
 					where: {
 						id: ctx.session.userId,
